e530c05317d97b1e3fe0d1a45cd2273e4ce1abbd
Edwin Ismail <typhoon_ed@yahoo.com>
2021-11-18 15:52:10 +0100

    :tada: addition of metrics and license APIs

--- implementation.py
+++ implementation.py
@@ -204,7 +204,8 @@ class DependencyTrackAPI(object):
                 return (f"The project could not be found", response.status_code)
 
     #all about lincese 
-    def list_license(self, pageSize = 100):
+    def get_list_license(self, pageSize = 100):
+        """Returns a list of all licenses with complete metadata for each license"""
         license_list=list()
         pageNumber = 1
         response = self.session.get(self.apicall + f"/v1/license", params = {"pageSize": pageSize,"pageNumber": pageNumber})
@@ -220,5 +221,247 @@ class DependencyTrackAPI(object):
         else:
             return (f"Unauthorized ", response.status_code)
 
-dt=DependencyTrackAPI("http://localhost:8081","Z0g4jvxF1Yek3R8balySFj5kGahjR3oj")
-print(dt.list_license())
+    def get_license(self, licenseId):
+        """
+        Returns specific license
+        licenseID: (string) The SPDX License ID of the license to retrieve
+        """
+        response = self.session.get(self.apicall + f"/v1/license/{licenseId}")
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            return (f"Unauthorized ", response.status_code)
+        elif response.status_code == 404:
+            return(f"The license could not be found ", response.status_code)
+        else:
+            return response.status_code
+        
+    def get_license_concise(self, pageSize = 100):
+        """Returns a concise listing of all licenses"""
+        license_list = list()
+        pageNumber = 1
+        response = self.session.get(
+            self.apicall + f"/v1/license/concise", params={"pageSize": pageSize, "pageNumber": pageNumber})
+        for lice in range(0, len(response.json())):
+            license_list.append(response.json()[lice-1])
+        while len(response.json()) == pageSize:
+            pageNumber += 1
+            response = self.session.get(
+                self.apicall + f"/v1/license/concise", params={"pageSize": pageSize, "pageNumber": pageNumber})
+            for lice in range(0, len(response.json())):
+                license_list.append(response.json()[lice-1])
+        if response.status_code == 200:
+            return license_list
+        else:
+            return (f"Unauthorized ", response.status_code)
+    
+    #Metrics 
+    
+    def get_all_metrics(self, pageSize = 100):
+        """
+        Returns the sum of all vulnerabilities in the database by year and month
+        """
+        metrics_list = list()
+        pageNumber = 1
+        response = self.session.get(self.apicall +f"/v1/metrics/vulnerability", params = {"pageSize": pageSize,"pageNumber": pageNumber})
+        for metric in range(0, len(response.json())):
+            metrics_list.append(response.json()[metric-1])
+        while len(response.json()) == pageSize:
+            pageNumber += 1
+            response = self.session.get(self.apicall +f"/v1/metrics/vulnerability", params = {"pageSize": pageSize,"pageNumber": pageNumber})
+            for metric in range(0, len(response.json())):
+                metrics_list.append(response.json()[metric-1])
+        if response.status_code == 200:
+            return metrics_list
+        else:
+            return (f"Unauthorized ", response.status_code)
+
+    def get_metrics_portolio_bydate(self, date):
+        """ 
+        Returns historical metrics for the entire portfolio from a specific date.
+        date: The start date to retrieve metric. Date format must be YYYYMMDD
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/portfolio/since/{date}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return (f"Unauthorized", response.status_code)
+        
+    def get_metrics_project_bydate(self, uuid, date):
+        """ 
+        Returns historical metrics for a specific project from a specific date
+        date: The start date to retrieve metric. Date format must be YYYYMMDD.
+        uuid: The UUID of the project to retrieve metrics for.
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/project/{uuid}/since/{date}")
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            return (f"Unauthorized ", response.status_code)
+        elif response.status_code == 403:
+            return (f"Access to the specified project is forbidden ", response.status_code)
+        elif response.status_code == 404:
+            return (f"Project not found", response.status_code)
+        else:
+            return (response.status_code)
+
+    def get_current_metrics_portfolio(self):
+        """
+        Returns current metrics for entire portfolio
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/portfolio/current")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return (f"Unauthorized ", response.status_code)
+
+    def get_metrics_dayNumber(self, days):
+        """ 
+        Returns X days of historical metrics for the entire portfolio(int32)
+        days: The number of days back to retrieve metrics for.
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/portfolio/{days}/days")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return (f"Unauthorized ", response.status_code)
+
+    def get_metrics_refresh_portfolio(self):
+        """ 
+        Requests a refresh of the portfolio metrics
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/portfolio/refresh")
+        if response.status_code == 200:
+            return (f"successful operation ", response.status_code)
+        else:
+            return (f"Unauthorized ", response.status_code)
+
+    def get_metrics_specific_project(self, uuid):
+        """ 
+        returns current metrics for a specific project.
+        uuid: The UUID of the project to retrieve metrics for
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/project/{uuid}/current")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return (f"Unauthorized ", response.status_code)
+        
+    def get_metrics_specific_project_days(self, uuid, days):
+        """ 
+        Returns X days of historical metrics for a specific project
+        uuid: The UUID of the project to retrieve metrics for.
+        days: The number of days back to retrieve metrics for.
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/project/{uuid}/days/{days}")
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            return (f"Unauthorized ", response.status_code)
+        elif response.status_code == 403:
+            return (f"Access to the specified project is forbidden ", response.status_code)
+        elif response.status_code == 404:
+            return (f"Project not found", response.status_code)
+        else:
+            return (response.status_code)
+
+    def get_metrics_refresh_project(self, uuid):
+        """ 
+        requests a refresh of a specific project metrics.
+        uuid: The UUID of the project to retrieve metrics for.
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/project/{uuid}/refresh")
+        if response.status_code == 200:
+            return (f"successful operation ", response.status_code)
+        elif response.status_code == 401:
+            return (f"Unauthorized ", response.status_code)
+        elif response.status_code == 403:
+            return (f"Access to the specified project is forbidden ", response.status_code)
+        elif response.status_code == 404:
+            return (f"Project not found", response.status_code)
+        else:
+            return (response.status_code)
+
+    def get_current_metrics_component(self, uuid):
+        """ 
+        Returns current metrics for a specific component
+        uuid: The UUID of the component to retrieve metrics for.
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/component/{uuid}/current")
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            return (f"Unauthorized ", response.status_code)
+        elif response.status_code == 403:
+            return (f"Access to the specified project is forbidden ", response.status_code)
+        elif response.status_code == 404:
+            return (f"Project not found", response.status_code)
+        else:
+            return (response.status_code)
+    
+    def get_metrics_component_bydate(self, uuid, date, pageSize = 100):
+        """
+        Returns historical metrics for a specific component from a specific date
+
+        Args:
+            uuid (string): The UUID of the component to retrieve metrics for.
+            date (string): The start date to retrieve metrics for.(Date format must be YYYYMMDD)
+        """
+        metrics_list = list()
+        pageNumber = 1
+        response = self.session.get(self.apicall +f"/v1/metrics/component/{uuid}/since/{date}", params = {"pageSize": pageSize, "pageNumber": pageNumber})
+        for metric in range(0, len(response.json())):
+            metrics_list.append(response.json()[metric-1])
+        while len(response.json()) == pageSize:
+            pageNumber += 1
+            response = self.session.get(self.apicall + f"/v1/metrics/component/{uuid}/since/{date}", params = {"pageSize": pageSize,"pageNumber": pageNumber})
+            for metric in range(0, len(response.json())):
+                metrics_list.append(response.json()[metric-1])
+        if response.status_code == 200:
+            return metrics_list
+        else:
+            return (f"Unauthorized ", response.status_code)
+        
+    def get_metrics_component_bydays(self, uuid, days, pageSize=100):
+        """
+        Returns X days of historical metrics for a specific component
+
+        Args:
+            uuid (string): The UUID of the component to retrieve metrics for.
+            days (int32): The number of days back to retrieve metrics for.
+        """
+        metrics_list = list()
+        pageNumber = 1
+        response = self.session.get(self.apicall + f"/v1/metrics/component/{uuid}/since/{days}", params={"pageSize": pageSize, "pageNumber": pageNumber})
+        for metric in range(0, len(response.json())):
+            metrics_list.append(response.json()[metric-1])
+        while len(response.json()) == pageSize:
+            pageNumber += 1
+            response = self.session.get(self.apicall + f"/v1/metrics/component/{uuid}/since/{days}", params={"pageSize": pageSize, "pageNumber": pageNumber})
+            for metric in range(0, len(response.json())):
+                metrics_list.append(response.json()[metric-1])
+        if response.status_code == 200:
+            return metrics_list
+        else:
+            return (f"Unauthorized ", response.status_code)
+
+    def get_metrics_component_refresh(self, uuid):
+        """[Requests a refresh of a specific components metrics]
+
+        Args:
+            uuid ([string]): [The UUID of the component to retrieve metrics for.]
+
+        Returns:
+            [string]: [status code]
+        """
+        response = self.session.get(self.apicall +f"/v1/metrics/component/{uuid}/refresh")
+        if response.status_code == 200:
+            return (f"successful operation ", response.status_code)
+        elif response.status_code == 401:
+            return (f"Unauthorized ", response.status_code)
+        elif response.status_code == 403:
+            return (f"Access to the specified project is forbidden ", response.status_code)
+        elif response.status_code == 404:
+            return (f"Project not found", response.status_code)
+        else:
+            return (response.status_code)
